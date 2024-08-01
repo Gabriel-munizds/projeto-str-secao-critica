21cc199c0a4a53d9a40548b33c2e43c1f0411fb9
João Gabriel Muniz da Silva <infraestrutura@d2ti.com.br>
2024-08-01 09:22:29 -0300

    estrutura inicial projeto spring

---
+++ str/src/test/java/br/com/ufma/str/StrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufma.str;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
